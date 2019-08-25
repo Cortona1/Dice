4a565437cb04c0b0e886d9b58e60e37ce8404d74
Cortona1 <anthonycorton95@gmail.com>
2019-08-25 15:18:55 -0400

    test

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,6 +1,30 @@
 
+import javax.swing.text.html.CSS;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DiceRoll {
+    private Random random;
+    private List<Integer> rollResults;
+
+
+    public DiceRoll() {
+        this.random = new Random();
+        rollResults = new ArrayList<Integer>();
+
+        // We are going to just go ahead and calculate the six dice rolls in the constructor
+
+        for (int i = 0; i < 6; i++) {
+            this.rollResults.add(this.random.nextInt(7));
+
+        }
+    }
+
+    public List<Integer> getRolls() {
+        return this.rollResults;
+
+    }
+
 
 }
--- src/Player.java
+++ src/Player.java
@@ -12,4 +12,19 @@ public class Player {
         score = new HashMap<Integer, DiceRoll>();
 
     }
+
+    public String getName() {
+        return this.name;
+
+    }
+
+    public DiceRoll getDiceRoll(Integer round) {
+        return this.score.get(round);
+    }
+
+    public void playerRollsDice(Integer round) {
+        DiceRoll newRoll = new DiceRoll();
+        score.put(round, newRoll);
+
+    }
 }
