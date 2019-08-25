34faad1a6c2e2fd361b7caa9d21df115c144c3fb
Cortona1 <anthonycorton95@gmail.com>
2019-08-25 15:04:19 -0400

    test

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -13,18 +13,20 @@ public class UserInterface {
 
     }
     public void Start() {
-        System.out.println("Lets play a game of dice, the highest score wins\n");
+        System.out.println("\nLets play a game of dice, the highest score wins\n" + "\nRolling higher numbers will give you"
+                + " more points the player with the most points win\n");
         askUser();
+        rollDice();
 
 
     }
 
     public void askUser() {
-        System.out.println("");
-
+        System.out.println("Please enter the names of all players individually.\n" + "Pressing enter with nothing typed"
+                + " will bring you to the rolling phase\n" );
 
         while(true) {
-            System.out.print("Enter the name of the players: ");
+            System.out.print("  Enter the name of the players: ");
             String userInput = reader.nextLine();
 
             if (userInput.equals("")) {
@@ -35,5 +37,11 @@ public class UserInterface {
             }
 
         }
+    }
+
+    public void rollDice() {
+        int round = 1;
+
+
     }
 }
